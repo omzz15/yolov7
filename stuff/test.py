a5801b3ddf4cabc79f897af9011cd5e52e9587e0
omzz15 <drompatel5@gmail.com>
2023-01-21 16:57:31 -0500

    trained model

---
+++ stuff/test.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import torch
+
+from models.yolo import Model
+from utils.general import check_requirements, set_logging
+from utils.google_utils import attempt_download
+from utils.torch_utils import select_device
+
+dependencies = ['torch', 'yaml']
+check_requirements(Path("/content/yolov7/").parent / 'requirements.txt', exclude=('pycocotools', 'thop'))
+set_logging()
+
+def custom(path_or_model='C:/Development/Robotics/FRC/Test_Vision/ML/yolov7/stuff/best.pt', autoshape=True):
+    """custom mode
+
+    Arguments (3 options):
+        path_or_model (str): 'path/to/model.pt'
+        path_or_model (dict): torch.load('path/to/model.pt')
+        path_or_model (nn.Module): torch.load('path/to/model.pt')['model']
+
+    Returns:
+        pytorch model
+    """
+    model = torch.load(path_or_model, map_location=torch.device('cpu')) if isinstance(path_or_model, str) else path_or_model  # load checkpoint
+    if isinstance(model, dict):
+        model = model['ema' if model.get('ema') else 'model']  # load model
+
+    hub_model = Model(model.yaml).to(next(model.parameters()).device)  # create
+    hub_model.load_state_dict(model.float().state_dict())  # load state_dict
+    hub_model.names = model.names  # class names
+    if autoshape:
+        hub_model = hub_model.autoshape()  # for file/URI/PIL/cv2/np inputs and NMS
+    device = select_device('0' if torch.cuda.is_available() else 'cpu')  # default to GPU if available
+    return hub_model.to(device)
+
+model = custom()  # custom example
+# model = create(name='yolov7', pretrained=True, channels=3, classes=80, autoshape=True)  # pretrained example
+
+print(model)
